da25ce6b08b94b2fdc0b3e2aba1c4a398f8d13dd
Bruno Vinícius <vbruno96@gmail.com>
2023-12-02 14:48:59 -0300

    fix css partner list block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'ab778ce1255e1c32cd50');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'fe12d01df235015e7f50');
